efdc899460b8fe00c8b888f80427a7c279d2a5ae
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-21 14:03:45 +0200

    Add project files.

---
+++ upgift 4._26/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Upgift4._26
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+
+
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra != 9)
+                    Console.WriteLine(siffra + 1);
+                else if (siffra == 9)
+                    Console.WriteLine(siffra = 0);
+
+            }
+        }
+    }
+}
+
